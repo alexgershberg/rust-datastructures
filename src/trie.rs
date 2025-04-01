cceae8fda5649c8d213f5c45de33e5ced3b08e37
Alex Gershberg <aleksandr.gershberg99@gmail.com>
2025-04-02 00:34:43 +0100

    Trie contains

--- src/trie.rs
+++ src/trie.rs
@@ -84,7 +84,22 @@ impl Trie {
     }
 
     pub fn contains(&self, text: &str) -> bool {
-        todo!()
+        let Some(root) = self.root else {
+            return false;
+        };
+
+        let mut current = root;
+        for c in text.chars() {
+            let node = unsafe { &*current.as_ptr() };
+            let children = &node.children;
+            let Some(&node) = children.get(&c) else {
+                return false;
+            };
+            current = node;
+        }
+
+        let current = unsafe { &*current.as_ptr() };
+        current.terminal
     }
 }
 
@@ -157,9 +172,8 @@ fn print_trie(trie: &Trie) {
 
 #[cfg(test)]
 mod tests {
-    use crate::trie::{Node, Trie, fmt_ptr, print_trie};
+    use crate::trie::{Node, Trie, print_trie};
     use std::collections::HashMap;
-    use std::fmt::Formatter;
     use std::ptr::NonNull;
 
     #[test]
@@ -186,13 +200,90 @@ mod tests {
     #[test]
     fn insert_1() {
         let mut trie = Trie::new();
-        trie.insert("Hello");
-        trie.insert("Stop");
-        trie.insert("Help");
-        trie.insert("Heron");
-        trie.insert("Stuff");
-        trie.insert("Alex");
+        assert!(trie.insert("Hello"));
+        assert!(trie.insert("Stop"));
+        assert!(trie.insert("Help"));
+        assert!(trie.insert("Heron"));
+        assert!(trie.insert("Stuff"));
+        assert!(trie.insert("Alex"));
         print_trie(&trie);
         // println!("{trie:?}");
+    }
+
+    #[test]
+    fn insert_2() {
+        let mut trie = Trie::new();
+        assert!(trie.insert("Hello"));
+        assert!(trie.insert("World"));
+        assert!(trie.insert("Hell"));
+        assert!(!trie.insert("Hello"));
+    }
+
+    #[test]
+    fn contains_1() {
+        let mut trie = Trie::new();
+        assert!(trie.insert("Hello"));
+        assert!(trie.contains("Hello"));
+    }
+
+    #[test]
+    fn contains_2() {
+        let trie = Trie::new();
+        assert!(!trie.contains("Hello"));
+    }
+
+    #[test]
+    fn contains_3() {
+        let mut trie = Trie::new();
+        assert!(trie.insert("Hello"));
+        assert!(!trie.contains("Hell"));
+        assert!(!trie.contains("Help"));
+        assert!(!trie.contains("E"));
+        assert!(!trie.contains("LONG SEQUENCE OF CHARACTERS"));
+        assert!(!trie.contains(""));
+        assert!(trie.contains("Hello"));
+    }
+
+    #[test]
+    fn contains_4() {
+        let mut trie = Trie::new();
+        assert!(trie.insert("Apple"));
+        assert!(trie.insert("Adam"));
+        assert!(trie.insert("Garden"));
+        assert!(trie.insert("Gravel"));
+        assert!(trie.insert("Ground"));
+        assert!(trie.insert("Axel"));
+        assert!(trie.insert("Alex"));
+        assert!(trie.insert("Alexander"));
+
+        assert!(trie.contains("Apple"));
+        assert!(trie.contains("Adam"));
+        assert!(trie.contains("Garden"));
+        assert!(trie.contains("Gravel"));
+        assert!(trie.contains("Ground"));
+        assert!(trie.contains("Axel"));
+        assert!(trie.contains("Alex"));
+        assert!(trie.contains("Alexander"));
+
+        assert!(!trie.contains("Ale"));
+        assert!(!trie.contains("Gravity"));
+        assert!(!trie.contains("Grav"));
+        assert!(!trie.contains("Gro"));
+        assert!(!trie.contains("G"));
+        assert!(!trie.contains("Grounded"));
+    }
+
+    #[test]
+    fn remove_1() {
+        let mut trie = Trie::new();
+        let had_value_to_remove = trie.remove("Nothing");
+        assert!(!had_value_to_remove);
+    }
+
+    #[test]
+    fn remove_2() {
+        let mut trie = Trie::new();
+        assert!(trie.insert("Hello"));
+        assert!(trie.remove("Hello"));
     }
 }
