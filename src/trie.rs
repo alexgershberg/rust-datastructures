640758c3c7d34875a83d5a9aa17b0f62475992ff
Alex Gershberg <aleksandr.gershberg99@gmail.com>
2025-04-02 00:15:41 +0100

    Debug repr wip

--- src/trie.rs
+++ src/trie.rs
@@ -1,11 +1,7 @@
 use std::collections::HashMap;
 use std::collections::hash_map::Entry;
-use std::fmt::{Debug, Formatter};
-use std::ptr::NonNull;
-
-pub struct Trie {
-    root: Option<NonNull<Node>>,
-}
+use std::fmt::{Debug, Formatter, Write};
+use std::ptr::{NonNull, write};
 
 struct Node {
     children: HashMap<char, NonNull<Node>>,
@@ -23,6 +19,28 @@ impl Node {
     fn new_non_null() -> NonNull<Self> {
         unsafe { NonNull::new_unchecked(Box::into_raw(Box::new(Node::new()))) }
     }
+}
+
+impl Debug for Node {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let children_debug_output = self
+            .children
+            .iter()
+            .map(|(c, ptr)| {
+                let node = unsafe { &*ptr.as_ptr() };
+                (c, (ptr, node))
+            })
+            .collect::<HashMap<_, _>>();
+
+        f.debug_struct("Node")
+            .field("children", &children_debug_output)
+            .field("terminal", &self.terminal)
+            .finish()
+    }
+}
+
+pub struct Trie {
+    root: Option<NonNull<Node>>,
 }
 
 impl Trie {
@@ -70,6 +88,21 @@ impl Trie {
     }
 }
 
+impl Debug for Trie {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let mut debug_struct = f.debug_struct("Trie");
+
+        if let Some(root) = &self.root {
+            let tup = (root, unsafe { &*root.as_ptr() });
+            debug_struct.field("root", &tup);
+        } else {
+            debug_struct.field("root", &self.root);
+        }
+
+        debug_struct.finish()
+    }
+}
+
 impl Drop for Trie {
     fn drop(&mut self) {
         let Some(root) = self.root.take() else { return };
@@ -89,31 +122,65 @@ impl Drop for Trie {
     }
 }
 
-impl Debug for Trie {
-    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        f.debug_struct("Trie").field("root", &self.root).finish()
-    }
+fn fmt_ptr<T>(ptr: NonNull<T>, f: &mut Formatter<'_>) -> std::fmt::Result
+where
+    T: Debug,
+{
+    f.debug_map()
+        .entry(&ptr, unsafe { &*ptr.as_ptr() })
+        .finish()
+}
+
+fn print_node(node: &Node) {
+    let debug = node
+        .children
+        .iter()
+        .map(|(c, ptr)| {
+            let node = unsafe { &*ptr.as_ptr() };
+            print_node(node);
+            c
+        })
+        .collect::<Vec<_>>();
+    println!("{:?}", debug);
 }
 
 fn print_trie(trie: &Trie) {
     println!("root: {:?}", trie.root);
-
     if let Some(root) = trie.root {
         let node = unsafe { root.as_ref() };
-        println!("children: {:#?}", node.children);
+        for (char, &ptr) in &node.children {
+            println!("{char}: ({ptr:?}: {:#?})", unsafe { &*(ptr.as_ptr()) });
+        }
         println!("terminal: {}", node.terminal)
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::trie::{Trie, print_trie};
+    use crate::trie::{Node, Trie, fmt_ptr, print_trie};
+    use std::collections::HashMap;
+    use std::fmt::Formatter;
+    use std::ptr::NonNull;
 
     #[test]
-    fn debug_1() {
+    fn debug_node() {
+        let node = Node {
+            children: HashMap::new(),
+            terminal: true,
+        };
+        let ptr = unsafe { NonNull::new_unchecked(Box::into_raw(Box::new(node))) };
+        let node = Node {
+            children: HashMap::from([('C', ptr)]),
+            terminal: false,
+        };
+        println!("{node:#?}");
+    }
+
+    #[test]
+    fn debug_trie() {
         let mut trie = Trie::new();
         trie.insert("Alex");
-        println!("{trie:#?}");
+        println!("{:#?}", &trie);
     }
 
     #[test]
@@ -126,5 +193,6 @@ mod tests {
         trie.insert("Stuff");
         trie.insert("Alex");
         print_trie(&trie);
+        // println!("{trie:?}");
     }
 }
